530639e0b8c2bdd76b164417dbf198f82e1a7739
heloisethero <thero.heloise@gmail.com>
2020-06-29 12:12:12 +0200

    reconfigure the repo architecture

--- src/clean_data.py
+++ src/clean_data.py
@@ -22,14 +22,12 @@ def import_data(RAW_DATA_DIRECTORY, DATE):
     return url_df, fact_check_df
 
 
-def clean_data(url_df, fact_check_df, SCIENTIFIC_TOPIC):
-    """Clean and merge the appearance data"""
-
-    # Remove the spaces added by error arount the URLs
-    url_df['url'] = url_df['url'].transform(lambda x: x.strip())
-
-    # Filter the URLs to keep only the ones flagged as False or equivalent:
+def keep_only_fake_url(url_df):
     url_df = url_df[(url_df['Flag as'].isin(['False', 'Partly false', 'Misleading', 'False headline']))]
+    return url_df
+
+
+def keep_only_one_topic(url_df, fact_check_df, SCIENTIFIC_TOPIC):
 
     # Use a REGEX to get the article field from the fact-check url website:
     # if the fact-check url starts with 'https://climatefeedback.org' -> 'climate' article
@@ -46,6 +44,15 @@ def clean_data(url_df, fact_check_df, SCIENTIFIC_TOPIC):
     url_df.loc[url_df['topic'] == 'COVID-19', 'field'] = 'COVID-19'
     url_df = url_df.dropna(subset=['field'])
     url_df = url_df[url_df['field'] == SCIENTIFIC_TOPIC]
+
+    return url_df
+
+
+def clean_url(url_df):
+    """Clean and merge the appearance data"""
+
+    # Remove the spaces added by error arount the URLs
+    url_df['url'] = url_df['url'].transform(lambda x: x.strip())
 
     # Clean the URLs and extract its domain name:
     url_df['url'] = url_df['url'].apply(lambda x: ural.normalize_url(x, 
@@ -64,17 +71,17 @@ def clean_data(url_df, fact_check_df, SCIENTIFIC_TOPIC):
     # # Remove the url with parameters from the analysis because CT return wrong results for them:
     # url_df['parameter_in_url'] = url_df['url'].apply(lambda x: '?' in x)
     # url_df = url_df[url_df['parameter_in_url']==False]
-
-    url_df = url_df[['url', 'Item reviewed', 'field', 'domain_name']]
     
     return url_df
-
+    
 
 def save_data(url_df, SCIENTIFIC_TOPIC, CLEAN_DATA_DIRECTORY, DATE):
-    """Save the clean CSV"""
+    url_df = url_df[['url', 'Item reviewed', 'field', 'domain_name']]
+
     clean_url_path = os.path.join(".", CLEAN_DATA_DIRECTORY, 
                                   "fake_url_" + SCIENTIFIC_TOPIC + "_" + DATE + ".csv")
     url_df.to_csv(clean_url_path, index=False)
+    print("The 'fake_url_{}_{}.csv' file has been saved in the 'clean_data' folder.".format(SCIENTIFIC_TOPIC, DATE))
 
 
 if __name__ == "__main__":
@@ -99,6 +106,8 @@ if __name__ == "__main__":
     CLEAN_DATA_DIRECTORY = "clean_data"
 
     url_df, fact_check_df = import_data(RAW_DATA_DIRECTORY, DATE)
-    url_df = clean_data(url_df, fact_check_df, SCIENTIFIC_TOPIC)
+    url_df = keep_only_fake_url(url_df)
+    url_df = keep_only_one_topic(url_df, fact_check_df, SCIENTIFIC_TOPIC)
+    url_df = clean_url(url_df)
     save_data(url_df, SCIENTIFIC_TOPIC, CLEAN_DATA_DIRECTORY, DATE)
-    print("The 'fake_url_{}_{}.csv' file has been saved in the 'clean_data' folder.".format(SCIENTIFIC_TOPIC, DATE))
+    
