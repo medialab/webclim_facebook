e58d464f2d305d0dec1695a40bdde492b33207aa
heloisethero <thero.heloise@gmail.com>
2020-07-20 17:27:25 +0200

    note the collect details

--- src/collect_crowdtangle_data_by_group.sh
+++ src/collect_crowdtangle_data_by_group.sh
@@ -11,8 +11,8 @@ minet ct posts --token $token_crowdtangle --list-ids 1401873 --start-date 2019-0
   --rate-limit 50 --partition-strategy 500 > $output_file
 
 ## 1 : 218 : https://www.facebook.com/groups/698141807656499 (0h53)
-## 2 : 148 : https://www.facebook.com/groups/346378325942318 (XhXX)
+## 2 : 148 : https://www.facebook.com/groups/346378325942318 (1h37)
 ## 3 : entre 20 et 98 : 15 groupes (5h03)
+## 4 : entre 10 et 11 : 20 groupes (2h26)
 ## 5 : entre 15 et 19 : 12 groupes (2h43)
 ## 6 : entre 12 et 14 : 16 groupes (1h54)
-## 7 : entre 10 et 11 : 20 groupes (2h26)
