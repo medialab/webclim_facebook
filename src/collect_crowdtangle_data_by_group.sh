21ae241f97581595fbb11975968250ac8450212f
heloisethero <thero.heloise@gmail.com>
2020-07-20 12:46:00 +0200

    add informations about last collect

--- src/collect_crowdtangle_data_by_group.sh
+++ src/collect_crowdtangle_data_by_group.sh
@@ -10,6 +10,9 @@ token_crowdtangle=$(jq -r '.token_crowdtangle' config.json)
 minet ct posts --token $token_crowdtangle --list-ids 1401873 --start-date 2019-09-01 \
   --rate-limit 50 --partition-strategy 500 > $output_file
 
-## 218 : https://www.facebook.com/groups/698141807656499 (0h53)
-## 148 : https://www.facebook.com/groups/346378325942318 (1h17) stoppée à date=2020-04-06T17:55:38
-## collect between  XX and  XX fake news shared : X groups (XhXX)
+## 1 : 218 : https://www.facebook.com/groups/698141807656499 (0h53)
+## 2 : 148 : https://www.facebook.com/groups/346378325942318 (XhXX)
+## 3 : entre 20 et 98 : 15 groupes (5h03)
+## 5 : entre 15 et 19 : 12 groupes (2h43)
+## 6 : entre 12 et 14 : 16 groupes (1h54)
+## 7 : entre 10 et 11 : 20 groupes (2h26)
