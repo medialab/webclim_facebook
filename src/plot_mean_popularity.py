f8522e1cf478619c1bb3be975ba51404d48d21ac
heloisethero <thero.heloise@gmail.com>
2020-07-22 16:08:34 +0200

    add the mean popularity per posts to the global figure

--- src/plot_mean_popularity.py
+++ src/plot_mean_popularity.py
@@ -15,6 +15,8 @@ def import_data(DATE):
 
 def details_figure(title):
 
+    plt.axvline(x=np.datetime64("2020-06-09"), color='grey', linestyle='--')
+
     plt.legend()
 
     plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%d-%m-%Y'))
@@ -23,13 +25,13 @@ def details_figure(title):
     plt.xlim(np.datetime64('2019-08-26'), np.datetime64('2020-07-18'))
     plt.ylim(bottom=0)
 
-    plt.title("The average " + title + " for the {} Facebook groups".format(posts_df.account_id.nunique()))
+    plt.title(title)
 
 
 def plot_figure(posts_df, DATE):
 
-    plt.figure(figsize=(12, 10))
-    plt.subplot(211)
+    plt.figure(figsize=(12, 15))
+    plt.subplot(311)
 
     plt.plot(posts_df.groupby(by=["date"])["reaction"].sum()/posts_df.account_id.nunique(), 
             label="Mean number of reactions per day")
@@ -40,14 +42,30 @@ def plot_figure(posts_df, DATE):
     plt.plot(posts_df.groupby(by=["date"])["share"].sum()/posts_df.account_id.nunique(), 
             label="Mean number of shares per day")
 
-    details_figure(title="daily popularity")
+    details_figure(title="The average daily popularity for the {} Facebook groups"
+                       .format(posts_df.account_id.nunique()))
 
-    plt.subplot(212)
+    plt.subplot(312)
 
     plt.plot(posts_df["date"].value_counts().sort_index()/posts_df.account_id.nunique(), 
         label="Mean number of posts per day", color="red")
 
-    details_figure(title="daily posts")
+    details_figure(title="The average daily posts for the {} Facebook groups"
+                       .format(posts_df.account_id.nunique()))
+
+    plt.subplot(313)
+
+    plt.plot(posts_df.groupby(by=["date"])["reaction"].mean(), 
+            label="Mean number of reactions per post")
+
+    plt.plot(posts_df.groupby(by=["date"])["comment"].mean(), 
+            label="Mean number of comments per post")
+
+    plt.plot(posts_df.groupby(by=["date"])["share"].mean(), 
+            label="Mean number of shares per post")
+
+    details_figure(title="The average popularity per post for the {} Facebook groups"
+                       .format(posts_df.account_id.nunique()))
 
     plt.tight_layout()
 
