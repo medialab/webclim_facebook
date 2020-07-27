1a81af2f3703e235cdd66585a1e96b03ca207293
heloisethero <thero.heloise@gmail.com>
2020-07-27 17:53:09 +0200

    add the code to resume the collect

--- src/collect_crowdtangle_data_by_group.sh
+++ src/collect_crowdtangle_data_by_group.sh
@@ -8,5 +8,8 @@ output_file="./data_crowdtangle_group/posts_group_${DATE}_${ITERATION}.csv"
 
 token_crowdtangle=$(jq -r '.token_crowdtangle' config.json)
 
+# minet ct posts --token $token_crowdtangle --list-ids $LIST --start-date 2019-09-01 \
+#   --rate-limit 50 --partition-strategy 500 > $output_file
+
 minet ct posts --token $token_crowdtangle --list-ids $LIST --start-date 2019-09-01 \
-  --rate-limit 50 --partition-strategy 500 > $output_file
+  --rate-limit 50 --partition-strategy 500 --resume --output $output_file
