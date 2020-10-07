72fc2842a7d385277c0ab791c1625bbbd0d9da50
heloisethero <thero.heloise@gmail.com>
2020-10-07 18:30:32 +0200

    create figure 2 to count the QAnon accounts sharing more than 3 fake news

--- src/web_conf_create_paper_figures_and_tables.py
+++ src/web_conf_create_paper_figures_and_tables.py
@@ -57,22 +57,92 @@ def filter_accounts_sharing_less_than_x_fake_news(df, x):
 
 
 def print_table_1(df_before, df_after):
+    
+    print('\n\nTABLE 1')
 
     df_before_filtered = filter_accounts_sharing_less_than_x_fake_news(df_before, x=10)
     print('\nThere are {} Facebook accounts sharing more than 10 fake news in the June data.'\
         .format(df_before_filtered.account_id.nunique()))
 
-
     list_qanon_before = [name for name in list(df_before_filtered.account_name.value_counts().index) if 'Q' in name]
     print('The list of the {} QAnon related accounts in the June data:'.format(len(list_qanon_before)))
-    print(*list_qanon_before, sep='\n')
+    print(df_before_filtered[df_before_filtered['account_name'].isin(list_qanon_before)]\
+        [['account_name', 'account_subscriber_count']].drop_duplicates())
+    print('Total number of follower: {}'.format(np.sum(df_before_filtered[df_before_filtered['account_name'].isin(list_qanon_before)]\
+        [['account_name', 'account_subscriber_count']].drop_duplicates()[['account_subscriber_count']].values)))
+
 
     df_after_filtered = filter_accounts_sharing_less_than_x_fake_news(df_after, x=10)
     print('\nThere are {} Facebook accounts sharing more than 10 fake news in the August data.'\
         .format(df_after_filtered.account_id.nunique()))
+
     list_qanon_after = [name for name in list(df_after_filtered.account_name.value_counts().index) if 'Q' in name]
     print('The list of the {} QAnon related accounts in the Augsut data:'.format(len(list_qanon_after)))
-    print(*list_qanon_after, sep='\n')
+    print(df_after_filtered[df_after_filtered['account_name'].isin(list_qanon_after)]\
+        [['account_name', 'account_subscriber_count']].drop_duplicates())
+    print('Total number of follower: {}'.format(np.sum(df_after_filtered[df_after_filtered['account_name'].isin(list_qanon_after)]\
+        [['account_name', 'account_subscriber_count']].drop_duplicates()[['account_subscriber_count']].values)))
+
+
+def print_table_2(df_before, df_after):
+
+    print('\n\nTABLE 2')
+
+    df_before_filtered = filter_accounts_sharing_less_than_x_fake_news(df_before, x=3)
+    print('\nThere are {} Facebook accounts sharing more than 3 fake news in the June data.'\
+        .format(df_before_filtered.account_id.nunique()))
+
+    list_qanon_before = [name for name in list(df_before_filtered.account_name.value_counts().index) if 'Q' in name]
+    accounts_to_remove_before = [
+        "Vương Nhất Bác 王一博 - UNIQ's Wang Yibo 1st Vietnamese Fanpage",
+        'QUESTION EVERYTHING... EVERYWHERE',
+        'QuantumEquilibrium ~ The Truth State',
+        '@STOP 5 G MONTRÉAL/QUÉBEC',
+        'AMERICANS Against Excessive Quarantine!',
+        'Questioning Global Warming',
+        'QUESTION EVERYTHING',
+        'Rastos Quimicos Portugal//Chemtrail Activism',
+        'Natural And Quodesh',
+        'STOP 5G Val-David. Laurentides et autres circonscriptions du Québec',
+        'Rockcastle County C.V.Q.R.G',
+        'Info Pro-Trump  Québec'
+    ]
+    list_qanon_before = [x for x in list_qanon_before if x not in accounts_to_remove_before]
+
+    print('There are {} accounts with Q in their names in the June data:'.format(len(list_qanon_before)))
+    print('Total number of follower: {}'.format(np.sum(df_before_filtered[df_before_filtered['account_name'].isin(list_qanon_before)]\
+        [['account_name', 'account_subscriber_count']].drop_duplicates()[['account_subscriber_count']].values)))
+
+
+    df_after_filtered = filter_accounts_sharing_less_than_x_fake_news(df_after, x=3)
+    print('\nThere are {} Facebook accounts sharing more than 3 fake news in the August data.'\
+        .format(df_after_filtered.account_id.nunique()))
+
+    list_qanon_after = [name for name in list(df_after_filtered.account_name.value_counts().index) if 'Q' in name]
+    accounts_to_remove_after = [
+        'QUESTION EVERYTHING... EVERYWHERE',
+        'AMERICANS Against Excessive Quarantine!',
+        'Info Pro-Trump  Québec',
+        'Médias Alternatifs Québecois',
+        'Wisconsinites Against Excessive Quarantine',
+        'Minnesotans Against Excessive Quarantine',
+        'Venner, der synes godt om Question Everything',
+        'Spiritual Awakening, Quantum Physics, Aliens & The 5th Dimension',
+        'Canadians Against Excessive Quarantine',
+        'PRQBUĐENI HRVATSKA',
+        'Quotes of Life',
+        'QUESTION EVERYTHING',
+        'Rastos Quimicos Portugal//Chemtrail Activism',
+        '@STOP 5 G MONTRÉAL/QUÉBEC',
+        'QuantumEquilibrium ~ The Truth State',
+        'STOP 5G Val-David. Laurentides et autres circonscriptions du Québec',
+        'Rockcastle County C.V.Q.R.G'
+    ]
+    list_qanon_after = [x for x in list_qanon_after if x not in accounts_to_remove_after]
+
+    print('There are {} accounts with Q in their names in the June data:'.format(len(list_qanon_after)))
+    print('Total number of follower: {}'.format(np.sum(df_after_filtered[df_after_filtered['account_name'].isin(list_qanon_after)]\
+        [['account_name', 'account_subscriber_count']].drop_duplicates()[['account_subscriber_count']].values)))
 
 
 def clean_crowdtangle_url_data(post_url_df, url_df):
@@ -192,7 +262,7 @@ def save_figure_1(posts_df, post_url_df, url_df, DATE):
     plt.figure(figsize=(12, 5))
 
     for group_index in range(4):
-        account_id = posts_groups_df[posts_groups_df['account_name']==accounts_to_plot[group_index]].account_id.unique()[0]
+        account_id = posts_df[posts_df['account_name']==accounts_to_plot[group_index]].account_id.unique()[0]
 
         ax = plt.subplot(2, 2, group_index + 1)
 
@@ -200,7 +270,7 @@ def save_figure_1(posts_df, post_url_df, url_df, DATE):
         for date in fake_news_dates:
             plt.axvline(x=date, color='C7', linestyle='-')
 
-        plot_one_group(posts_groups_df, account_id, plot_special_date=False, DATE=DATE)
+        plot_one_group(posts_df, account_id, plot_special_date=False, DATE=DATE)
         
         if group_index == 0: 
             plt.ylim([0, 20])
@@ -300,6 +370,7 @@ if __name__ == "__main__":
 
     df_before, df_after = clean_comparison_data(before_date="02_06_2020", after_date="2020-08-31")
     print_table_1(df_before, df_after)
+    print_table_2(df_before, df_after)
 
     url_df = import_data(folder="data_sciencefeedback", file_name="appearances_" + DATE + "_.csv")
 
