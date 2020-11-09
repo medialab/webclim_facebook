ee907c8684171598abcc2003bf297c5820d7b783
heloisethero <thero.heloise@gmail.com>
2020-11-09 17:55:27 +0100

    update the code to minet version 0.33

--- code/collect_crowdtangle_data_by_group.sh
+++ code/collect_crowdtangle_data_by_group.sh
@@ -1,24 +1,7 @@
 #!/bin/bash
 
 LIST=$1
-
 TODAY_DATE=$(date +"%Y-%m-%d")
-token_crowdtangle=$(jq -r '.token_crowdtangle' config.json)
 
-echo "starting data collection..."
-
-minet ct posts --token $token_crowdtangle --list-ids $LIST --start-date 2019-11-01 --end-date 2020-10-27 \
-  --rate-limit 50 --partition-strategy 500 > "./data/data_crowdtangle_group/posts_group_${TODAY_DATE}_0.csv"
-echo "2020 DONE!"
-
-minet ct posts --token $token_crowdtangle --list-ids $LIST --start-date 2018-11-01 --end-date 2019-11-01 \
-  --rate-limit 50 --partition-strategy 500 > "./data/data_crowdtangle_group/posts_group_${TODAY_DATE}_1.csv"
-echo "2019 DONE!"
-
-minet ct posts --token $token_crowdtangle --list-ids $LIST --start-date 2017-11-01 --end-date 2018-11-01 \
-  --rate-limit 50 --partition-strategy 500 > "./data/data_crowdtangle_group/posts_group_${TODAY_DATE}_2.csv"
-echo "2018 DONE!"
-
-minet ct posts --token $token_crowdtangle --list-ids $LIST --start-date 2016-11-01 --end-date 2017-11-01 \
-  --rate-limit 50 --partition-strategy 500 > "./data/data_crowdtangle_group/posts_group_${TODAY_DATE}_3.csv"
-echo "2017 DONE!"
+minet ct posts --list-ids $LIST --start-date 2016-11-01 --end-date 2020-10-31 > \
+  "./data/data_crowdtangle_group/posts_group_${TODAY_DATE}.csv"
--- code/collect_crowdtangle_data_by_url.sh
+++ code/collect_crowdtangle_data_by_url.sh
@@ -10,12 +10,4 @@ TODAY_DATE=$(date +"%Y-%m-%d")
 INPUT_FILE="./data/data_sciencefeedback/appearances_${DATE}_${TOPIC}.csv"
 OUTPUT_FILE="./data/data_crowdtangle_url/posts_url_${TODAY_DATE}_${TOPIC}.csv"
 
-token_crowdtangle=$(jq -r '.token_crowdtangle' config.json)
-
-if [ ! -f $OUTPUT_FILE ];
-then
-    minet ct summary url $INPUT_FILE --token $token_crowdtangle \
-     --posts $OUTPUT_FILE --sort-by total_interactions --start-date 2019-01-01
-else
-    echo "Nothing to do, the output file already exists"
-fi
+minet ct summary url $INPUT_FILE --posts $OUTPUT_FILE --sort-by total_interactions --start-date 2019-01-01
