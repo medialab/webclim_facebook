c8483705e4c42ceb7b2c2c207362f229909714bb
heloisethero <thero.heloise@gmail.com>
2020-11-10 12:19:04 +0100

    update to minet 0.34

--- code/collect_crowdtangle_data_by_url.sh
+++ code/collect_crowdtangle_data_by_url.sh
@@ -10,4 +10,5 @@ TODAY_DATE=$(date +"%Y-%m-%d")
 INPUT_FILE="./data/data_sciencefeedback/appearances_${DATE}_${TOPIC}.csv"
 OUTPUT_FILE="./data/data_crowdtangle_url/posts_url_${TODAY_DATE}_${TOPIC}.csv"
 
-minet ct summary url $INPUT_FILE --posts $OUTPUT_FILE --sort-by total_interactions --start-date 2019-01-01
+minet ct summary url $INPUT_FILE --posts $OUTPUT_FILE \
+ --sort-by total_interactions --start-date 2019-01-01 --platforms facebook
