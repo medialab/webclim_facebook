752b878c2775affbe5f266504153118eccad651f
Heloise Thero <thero.heloise@gmail.com>
2020-12-02 16:46:24 +0100

    plot new figure 1 and individual plots

--- code/create_blog_post_figure.py
+++ code/create_blog_post_figure.py
@@ -9,6 +9,13 @@ import matplotlib.dates as mdates
 
 from create_paper_tables_and_figures import import_data, save_figure
 
+
+def import_json(folder, file_name):
+    data_path = os.path.join('.', 'data', folder, file_name)
+    with open(data_path) as json_file:
+        data = json.load(json_file)
+    return data
+
 def clean_post_data(df):
 
     clean_df = pd.DataFrame(columns=[
@@ -36,32 +43,26 @@ def rolling_average_per_day(df, column):
     return df.groupby(by=["date"])[column].mean().rolling(window=5, win_type='triang', center=True).mean()
 
 
-def plot_one_group(posts_df, account_id, reduced_date, ax):
+def plot_one_group(posts_df, account_id, ax):
     
     posts_df_group = posts_df[posts_df["account_id"] == account_id] 
     plt.plot(rolling_average_per_day(posts_df_group, 'reaction'), label="Number of reactions per post")
     plt.plot(rolling_average_per_day(posts_df_group, 'comment'), label="Number of comments per post")
     plt.plot(rolling_average_per_day(posts_df_group, 'share'), label="Number of shares per post")
     
-    plt.axvline(x=np.datetime64(reduced_date), color='C3', linestyle='--', linewidth=2)
-
     plt.legend()
 
-    xticks = [np.datetime64('2019-01-01'), np.datetime64('2019-03-01'), np.datetime64('2019-05-01'), 
-               np.datetime64('2019-07-01'), np.datetime64('2019-09-01'), np.datetime64('2019-11-01'),
+    xticks = [np.datetime64('2019-11-01'),
                np.datetime64('2020-01-01'), np.datetime64('2020-03-01'), np.datetime64('2020-05-01'), 
-               np.datetime64('2020-07-01'), np.datetime64('2020-09-01'), np.datetime64('2020-11-01'),
-               np.datetime64(reduced_date)]
-    if account_id == 111077570271805:
-        xticks.pop(11)
+               np.datetime64('2020-07-01'), np.datetime64('2020-09-01'), np.datetime64('2020-11-01')]
     plt.xticks(xticks, rotation=30, ha='right')
-    plt.gca().get_xticklabels()[-1].set_color('red')
     
     plt.xlim(
-        np.datetime64(datetime.datetime.strptime('2019-01-01', '%Y-%m-%d') - datetime.timedelta(days=4)), 
+        np.datetime64(datetime.datetime.strptime('2019-11-01', '%Y-%m-%d') - datetime.timedelta(days=4)), 
         np.datetime64(datetime.datetime.strptime('2020-11-30', '%Y-%m-%d') + datetime.timedelta(days=4))
     )
     plt.ylim(bottom=0)
+    plt.locator_params(axis='y', nbins=3)
     
     ax.spines['right'].set_visible(False)
     ax.spines['left'].set_visible(False)
@@ -69,51 +70,64 @@ def plot_one_group(posts_df, account_id, reduced_date, ax):
     ax.grid(axis="y")
 
 
-def save_blog_figure(posts_df):
+def save_figure_1(posts_df, repeat_offender_date):
 
-    plt.figure(figsize=(9, 13))
+    account_name = 'Mark Levin'
+    account_id = posts_df[posts_df['account_name']==account_name].account_id.unique()[0]
+
+    plt.figure(figsize=(10, 4))
+    ax = plt.subplot()
+    
+    plt.title(account_name, size="x-large")
+
+    plot_one_group(posts_df, account_id, ax)
+
+    xticks = [np.datetime64('2019-11-01'),
+              np.datetime64('2020-01-01'), np.datetime64('2020-03-01'), np.datetime64('2020-05-01'), 
+              np.datetime64('2020-07-01'), np.datetime64('2020-09-01'), np.datetime64('2020-11-01'),
+              np.datetime64(repeat_offender_date[account_name])]
+    plt.xticks(xticks, rotation=30, ha='right')
+    plt.gca().get_xticklabels()[-1].set_color('red')
+
+    plt.axvline(x=np.datetime64(repeat_offender_date[account_name]), 
+                color='C3', linestyle='--', linewidth=2)
+
+    plt.tight_layout()
+    save_figure('figure_blog_1')
+
+
+def save_all_groups_figures(posts_df, repeat_offender_date):
+
     group_index = 0
 
-    for account_id in [311190048935167, 368557930146199, 16307558831, 111077570271805]:
+    for account_id in posts_df['account_id'].unique():
 
-        ax = plt.subplot(4, 1, group_index + 1)
+        if group_index % 10 == 0:
+            plt.figure(figsize=(15, 17))
+
+        ax = plt.subplot(5, 2, group_index % 10 + 1)
         
         account_name = posts_df[posts_df['account_id']==account_id].account_name.unique()[0]
         plt.title(account_name, size="x-large")
-        
-        if account_name == '100 Percent FED Up':
-            reduced_date = '2020-07-31'
-            plt.yticks([2500, 5000])
-            plt.ylim([0, 6000])
-        elif account_name == 'Tucker Carlson Tonight':
-            reduced_date = '2020-09-24'
-            plt.yticks([50000, 100000])
-        elif account_name == 'Mark Levin':
-            reduced_date = '2020-10-05'
-            plt.yticks([10000, 20000])
-        elif account_name == 'Wendy Bell Radio':
-            reduced_date = '2020-11-09'
 
-        plot_one_group(posts_df, account_id, reduced_date, ax)
-        
+        plot_one_group(posts_df, account_id, ax)
+        plt.axvline(x=np.datetime64(repeat_offender_date[account_name]), 
+                    color='C3', linestyle='--', linewidth=2)
+
         if group_index > 0: 
             ax.get_legend().set_visible(False)
+
+        if (group_index % 10 == 9) | (group_index == posts_df['account_id'].nunique() - 1):
+            plt.tight_layout()
+            save_figure('supplementary_figure_blog_{}'.format(int(group_index / 10) + 1))
+
         group_index += 1
-                
-    plt.tight_layout(pad=4.0)
-    save_figure('figure_blog_post')
 
 
 if __name__ == "__main__":
-    #collect_date = "2020-11-17"
     collect_date = "2020-12-01"
     posts_df = import_data(folder="data_crowdtangle_group", file_name='posts_group_' + collect_date + '.csv')
     posts_df = clean_post_data(posts_df)
-
-    print(posts_df.account_name.unique())
-
-    with open('./data/self_declared_repeat_offenders/dates.json') as json_file:
-        repeat_offender_date = json.load(json_file)
-    print(repeat_offender_date)
-
-    #save_blog_figure(posts_df)
+    repeat_offender_date = import_json(folder='self_declared_repeat_offenders', file_name='dates.json')
+    save_figure_1(posts_df, repeat_offender_date)
+    save_all_groups_figures(posts_df, repeat_offender_date)
