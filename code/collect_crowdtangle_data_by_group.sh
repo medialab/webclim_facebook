fbbcd71f992bb4bf32776030abfd3b4a74043c14
Heloise Thero <thero.heloise@gmail.com>
2021-01-05 18:51:53 +0100

    change collection dates

--- code/collect_crowdtangle_data_by_group.sh
+++ code/collect_crowdtangle_data_by_group.sh
@@ -3,5 +3,5 @@
 LIST=$1
 TODAY_DATE=$(date +"%Y-%m-%d")
 
-minet ct posts --list-ids $LIST --start-date 2019-01-01 > \
+minet ct posts --list-ids $LIST --start-date 2019-01-01 --end-date 2020-12-31 > \
   "./data/crowdtangle_group/posts_group_${TODAY_DATE}.csv"
