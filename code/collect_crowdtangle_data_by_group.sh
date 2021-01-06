85245082afceb3c2a21de5af11003bbe6ce14fbf
Heloise Thero <thero.heloise@gmail.com>
2021-01-06 12:29:41 +0100

    add resume command

--- code/collect_crowdtangle_data_by_group.sh
+++ code/collect_crowdtangle_data_by_group.sh
@@ -5,3 +5,6 @@ TODAY_DATE=$(date +"%Y-%m-%d")
 
 minet ct posts --list-ids $LIST --start-date 2019-01-01 --end-date 2020-12-31 > \
   "./data/crowdtangle_group/posts_group_${TODAY_DATE}.csv"
+
+# minet ct posts --list-ids $LIST --start-date 2019-01-01 --end-date 2020-12-31 \
+#   --resume --output "./data/crowdtangle_group/posts_group_${TODAY_DATE}.csv"
