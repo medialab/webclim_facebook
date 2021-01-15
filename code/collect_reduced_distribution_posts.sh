63cf60662838aba71dea6f5b0920b2dcb0ffe443
heloisethero <thero.heloise@gmail.com>
2021-01-15 11:40:20 +0100

    record the minet commands

--- code/collect_reduced_distribution_posts.sh
+++ code/collect_reduced_distribution_posts.sh
@@ -1,3 +1,6 @@
 minet ct search 'Your page has reduced distribution' \
     --start-date 2020-09-01 --end-date 2020-11-30 > \
     reduced_distribution_posts.csv
+
+# minet ct posts-by-id repeat-offender-post-url ./data/self_declared_repeat_offenders/supplementary_table_1.csv > \
+#     ./data/self_declared_repeat_offenders/posts.csv
